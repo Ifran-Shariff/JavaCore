44bbdc5f0022dec4d1d8fe8ceae181c8a0fd36a5
Ifran-Shariff <ifranshariff1998@gmail.com>
2026-06-27 18:05:27 +0530

    NEw changes by dev1

--- src/JAVA_Interview_programs/Learn_MergeConflict.java
+++ src/JAVA_Interview_programs/Learn_MergeConflict.java
@@ -3,6 +3,6 @@ package JAVA_Interview_programs;
 public class Learn_MergeConflict {
 
     public static void main(String[] args){
-        System.out.println("Hello world ifran");
+        System.out.println("Hello world ifran shariff");
     }
 }
